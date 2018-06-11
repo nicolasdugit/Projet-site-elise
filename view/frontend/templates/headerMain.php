2a0c5d8e8d6682a40d6295f9d1bf6e422a79dbfb
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-11 16:35:26 +0700

    init templates

---
+++ view/frontend/templates/headerMain.php
@@ -0,0 +1,140 @@
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="colorlib-navbar-brand">
+					<a class="colorlib-logo" href="index.html"><i class="flaticon-cutlery"></i><span>Lu</span><span>to</span></a>
+				</div>
+				<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+			</div>
+		</div>
+	</div>
+</header>
+<aside id="colorlib-hero">
+	<div class="flexslider">
+		<ul class="slides">
+	   	<li style="background-image: url(public/images/img_bg_1.jpg);">
+	   		<div class="overlay"></div>
+	   		<div class="container-fluid">
+	   			<div class="row">
+		   			<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+		   				<div class="slider-text-inner text-center">
+		   					<div class="desc">
+		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+			   					<h1>Special &amp; Fresh Food</h1>
+			   					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+			   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Book a table</a></p>
+			   					<div class="desc2"></div>
+		   					</div>
+		   				</div>
+		   			</div>
+		   		</div>
+	   		</div>
+	   	</li>
+	   	<li style="background-image: url(public/images/img_bg_2.jpg);">
+	   		<div class="overlay"></div>
+	   		<div class="container-fluid">
+	   			<div class="row">
+		   			<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+		   				<div class="slider-text-inner text-center">
+		   					<div class="desc">
+		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+			   					<h1>Exquisite Dishes From Chef</h1>
+			   					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+			   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Book a table</a></p>
+			   					<div class="desc2"></div>
+			   				</div>
+		   				</div>
+		   			</div>
+		   		</div>
+	   		</div>
+	   	</li>
+	   	<li style="background-image: url(public/images/img_bg_3.jpg);">
+	   		<div class="overlay"></div>
+	   		<div class="container-fluid">
+	   			<div class="row">
+		   			<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+		   				<div class="slider-text-inner text-center">
+		   					<div class="desc">
+		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+			   					<h1>We are Delicious Restaurant</h1>
+			   					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+			   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Book a table</a></p>
+			   					<div class="desc2"></div>
+			   				</div>
+		   				</div>
+		   			</div>
+		   		</div>
+	   		</div>
+	   	</li>
+	   	<li style="background-image: url(public/images/img_bg_4.jpg);">
+	   		<div class="overlay"></div>
+	   		<div class="container-fluid">
+	   			<div class="row">
+		   			<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+		   				<div class="slider-text-inner text-center">
+		   					<div class="desc">
+		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+			   					<h1>Book a table here in our site</h1>
+			   					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+			   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Book a table</a></p>
+			   					<div class="desc2"></div>
+			   				</div>
+		   				</div>
+		   			</div>
+		   		</div>
+	   		</div>
+	   	</li>	
+	  	</ul>
+	  	<div class="mouse">
+		   <a href="#" class="mouse-icon">
+		   	<div class="mouse-wheel"></div>
+		   </a>
+		</div>
+  	</div>
+</aside>
+
+<div class="colorlib-intro">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-6 text-center">
+				<div class="intro animate-box">
+					<span class="icon">
+						<i class="icon-map4"></i>
+					</span>
+					<h2>Address</h2>
+					<p>198 West 21th Street, Suite 721 New York NY 10016</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<div class="intro animate-box">
+					<span class="icon">
+						<i class="icon-clock4"></i>
+					</span>
+					<h2>Opening Time</h2>
+					<p>Monday - Sunday</p>
+					<span>8am - 9pm</span>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<div class="intro animate-box">
+					<span class="icon">
+						<i class="icon-mobile2"></i>
+					</span>
+					<h2>Phone</h2>
+					<p>+ 001 234 567</p>
+					<p>+ 001 234 567</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<div class="intro animate-box">
+					<span class="icon">
+						<i class="icon-envelope"></i>
+					</span>
+					<h2>Email</h2>
+					<p><a href="#">info@domain.com</a><br><a href="#">luto@email.com</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
