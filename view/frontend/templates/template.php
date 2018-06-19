8307bd4be5dfee16c7c4212500880e2b71a5ba03
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-19 10:51:58 +0700

    creation head.php

--- view/frontend/templates/template.php
+++ view/frontend/templates/template.php
@@ -1,61 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Luto Template</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="author" content="" />
-
-  <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url"public/ content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url"public/ content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="public/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="public/css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="public/css/bootstrap.css">
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="public/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="public/css/owl.theme.default.min.css">
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="public/css/magnific-popup.css">
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="public/css/flexslider.css">
-	<!-- Flaticons  -->
-	<link rel="stylesheet" href="public/fonts/flaticon/font/flaticon.css">
-	<!-- Date Picker -->
-	<link rel="stylesheet" href="public/css/bootstrap-datepicker.css">
-
-	<link rel="stylesheet" href="public/css/style.css">
-	
-	<!-- Fontawesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
-	<!-- Modernizr JS -->
-	<script src="public/js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
+		<?php include('view/frontend/templates/head.php'); ?>
 	</head>
 	<body>
 
