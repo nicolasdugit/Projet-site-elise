ee6211185b84600e6591e33122ffa06c50d0ffe3
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-19 10:34:49 +0700

    modification header et tests images

--- view/frontend/templates/headerMain.php
+++ view/frontend/templates/headerMain.php
@@ -2,8 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="colorlib-navbar-brand">
-					<a class="colorlib-logo" href="index.html"><i class="flaticon-cutlery"></i><span>Lu</span><span>to</span></a>
+				<div class="colorlib-navbar-brand text-center">
+					<!-- <a class="colorlib-logo" href="index.html"><i class="flaticon-cutlery"></i><span>Lu</span><span>to</span></a> -->
+					<a class="colorlib-logo" style="padding-left: 0px;padding-right: 0px; background-color: #DAF1FF" href="index.php"><img style="height: 100px;" src="public/images/logo-chez-elise.png"></a>
 				</div>
 				<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
 			</div>
@@ -13,7 +14,7 @@
 <aside id="colorlib-hero">
 	<div class="flexslider">
 		<ul class="slides">
-	   	<li style="background-image: url(public/images/img_bg_1.jpg);">
+	   	<li style="background-image: url(public/images/label1.jpg);">
 	   		<div class="overlay"></div>
 	   		<div class="container-fluid">
 	   			<div class="row">
@@ -21,8 +22,8 @@
 		   				<div class="slider-text-inner text-center">
 		   					<div class="desc">
 		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
-			   					<h1>Special &amp; Fresh Food</h1>
-			   					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+			   					<h1>Bienvenue à la Poissonnerie Du Port <br> Chez Elise</h1>
+			   					<p>Bienvenue à la Poissonnerie Du Port Chez Elise</p>
 			   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Book a table</a></p>
 			   					<div class="desc2"></div>
 		   					</div>
--- view/frontend/templates/template.php
+++ view/frontend/templates/template.php
@@ -45,7 +45,9 @@
 	<link rel="stylesheet" href="public/css/bootstrap-datepicker.css">
 
 	<link rel="stylesheet" href="public/css/style.css">
-
+	
+	<!-- Fontawesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 	<!-- Modernizr JS -->
 	<script src="public/js/modernizr-2.6.2.min.js"></script>
