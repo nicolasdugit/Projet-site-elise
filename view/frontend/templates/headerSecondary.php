dd8af0a87575ef48d12ccbf674dba29f7dbf164b
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-19 16:12:53 +0700

    creation du headersecondary pour pages annexes

---
+++ view/frontend/templates/headerSecondary.php
@@ -0,0 +1,40 @@
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="colorlib-navbar-brand text-center">
+				</div>
+				<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+			</div>
+		</div>
+	</div>
+</header>
+<aside id="colorlib-hero">
+	<div class="flexslider">
+		<ul class="slides">
+	   	<li style="background-image: url(public/images/label1.jpg);">
+	   		<div class="overlay"></div>
+	   		<div class="container-fluid">
+	   			<div class="row">
+		   			<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+		   				<div class="slider-text-inner text-center">
+		   					<div class="desc">
+		   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+			   					<h1>Bienvenue à la Poissonnerie Du Port <br> Chez Elise</h1>
+			   					<p>Toute l'année Elise et son équipe vous accueille sur le port de cassy</p>
+			   					<p><a href="index.php?page=contact" class="btn btn-primary btn-lg btn-learn">Où nous trouver ?</a></p>
+			   					<div class="desc2"></div>
+		   					</div>
+		   				</div>
+		   			</div>
+		   		</div>
+	   		</div>
+	   	</li>
+	   </ul>
+	  	<div class="mouse">
+		   <a href="#" class="mouse-icon">
+		   	<div class="mouse-wheel"></div>
+		   </a>
+		</div>
+  	</div>
+</aside>
