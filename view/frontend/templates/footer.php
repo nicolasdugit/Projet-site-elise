7e5ab2a9c16f6d9cd69a60c09340bb264610e350
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-20 10:35:51 +0700

    creation du pre-footer footer1.php

--- view/frontend/templates/footer.php
+++ view/frontend/templates/footer.php
@@ -2,62 +2,7 @@
 	<div id="footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3 col-pb-sm">
-					<h2>Lutong Bahay</h2>
-					<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind.</p>
-					<p class="colorlib-social-icons">
-						<a href="#"><i class="icon-facebook4"></i></a>
-						<a href="#"><i class="icon-twitter3"></i></a>
-						<a href="#"><i class="icon-googleplus"></i></a>
-						<a href="#"><i class="icon-dribbble2"></i></a>
-					</p>
-				</div>
-				<div class="col-md-3 col-pb-sm">
-					<h2>Latest Blog</h2>
-					<div class="f-entry">
-						<a href="#" class="featured-img" style="background-image: url(public/images/dish-5.jpg);"></a>
-						<div class="desc">
-							<span>May 5, 2018</span>
-							<h3><a href="#">How to cook beef Grilled with potato</a></h3>
-						</div>
-					</div>
-					<div class="f-entry">
-						<a href="#" class="featured-img" style="background-image: url(public/images/dish-7.jpg);"></a>
-						<div class="desc">
-							<span>May 5, 2018</span>
-							<h3><a href="#">A Japanese Master Chef</a></h3>
-						</div>
-					</div>
-					<div class="f-entry">
-						<a href="#" class="featured-img" style="background-image: url(public/images/dessert-3.jpg);"></a>
-						<div class="desc">
-							<span>May 5, 2018</span>
-							<h3><a href="#">Special Recipe for this month</a></h3>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3 col-pb-sm">
-					<h2>Instagram</h2>
-					<div class="instagram">
-						<a href="#" class="insta-img" style="background-image: url(public/images/dessert-1.jpg);"></a>
-						<a href="#" class="insta-img" style="background-image: url(public/images/dessert-2.jpg);"></a>
-						<a href="#" class="insta-img" style="background-image: url(public/images/dish-9.jpg);"></a>
-						<a href="#" class="insta-img" style="background-image: url(public/images/dish-2.jpg);"></a>
-					</div>
-				</div>
-				<div class="col-md-3 col-pb-sm">
-					<h2>Newsletter</h2>
-					<p>A small river named Duden flows by their place and supplies it with the necessary regelialia</p>
-					<div class="subscribe text-center">
-						<div class="form-group">
-							<input type="text" class="form-control text-center" placeholder="Enter Email address">
-							<input type="submit" value="Subscribe" class="btn btn-primary btn-custom">
-						</div>
-					</div>
-				</div>
-			</div>
-			<hr>
-			<div class="row">
+				<hr>
 				<div class="col-md-12 text-center">
 					<h4>Mentions Légales</h4>
 					<p>Entreprise Individuelle MADAME ELISE DUQUESNE (Poissonnerie du Port) <br>
