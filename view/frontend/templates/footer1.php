452c4968344aa2c70bc670b8e6bfde400bdf29c0
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-20 11:36:19 +0700

    rajout facebook dans footer1

--- view/frontend/templates/footer1.php
+++ view/frontend/templates/footer1.php
@@ -1,56 +1,64 @@
-<div class="container">
-	<div class="row">
-		<div class="col-md-3 col-pb-sm">
-			<h2>Lutong Bahay</h2>
-			<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind.</p>
-			<p class="colorlib-social-icons">
-				<a href="#"><i class="icon-facebook4"></i></a>
-				<a href="#"><i class="icon-twitter3"></i></a>
-				<a href="#"><i class="icon-googleplus"></i></a>
-				<a href="#"><i class="icon-dribbble2"></i></a>
-			</p>
-		</div>
-		<div class="col-md-3 col-pb-sm">
-			<h2>Latest Blog</h2>
-			<div class="f-entry">
-				<a href="#" class="featured-img" style="background-image: url(public/images/dish-5.jpg);"></a>
-				<div class="desc">
-					<span>May 5, 2018</span>
-					<h3><a href="#">How to cook beef Grilled with potato</a></h3>
+<div class="colorlib-about" style="background-color: grey;">
+	<div class="container">
+		<div class="row">
+			<!-- <div class="col-md-3 col-pb-sm">
+				<h2>Lutong Bahay</h2>
+				<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind.</p>
+				<p class="colorlib-social-icons">
+					<a href="#"><i class="icon-facebook4"></i></a>
+					<a href="#"><i class="icon-twitter3"></i></a>
+					<a href="#"><i class="icon-googleplus"></i></a>
+					<a href="#"><i class="icon-instagram"></i></a>
+				</p>
+			</div> -->
+			<div class="col-md-4 col-pb-sm">
+				<h2>Dernières recettes</h2>
+				<div class="f-entry">
+					<a href="#" class="featured-img" style="background-image: url(public/images/dish-5.jpg);"></a>
+					<div class="desc">
+						<span>May 5, 2018</span>
+						<h3><a href="#">How to cook beef Grilled with potato</a></h3>
+					</div>
+				</div>
+				<div class="f-entry">
+					<a href="#" class="featured-img" style="background-image: url(public/images/dish-7.jpg);"></a>
+					<div class="desc">
+						<span>May 5, 2018</span>
+						<h3><a href="#">A Japanese Master Chef</a></h3>
+					</div>
+				</div>
+				<div class="f-entry">
+					<a href="#" class="featured-img" style="background-image: url(public/images/dessert-3.jpg);"></a>
+					<div class="desc">
+						<span>May 5, 2018</span>
+						<h3><a href="#">Special Recipe for this month</a></h3>
+					</div>
 				</div>
 			</div>
-			<div class="f-entry">
-				<a href="#" class="featured-img" style="background-image: url(public/images/dish-7.jpg);"></a>
-				<div class="desc">
-					<span>May 5, 2018</span>
-					<h3><a href="#">A Japanese Master Chef</a></h3>
-				</div>
+			<div class="col-md-4 col-pb-sm">
+				<h2><a href="https://www.instagram.com/chez_elise_33138/"><i class="icon-instagram"></i></a> Instagram </h2>
+				
+				
 			</div>
-			<div class="f-entry">
-				<a href="#" class="featured-img" style="background-image: url(public/images/dessert-3.jpg);"></a>
-				<div class="desc">
-					<span>May 5, 2018</span>
-					<h3><a href="#">Special Recipe for this month</a></h3>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3 col-pb-sm">
-			<h2>Instagram</h2>
-			<div class="instagram">
-				<a href="#" class="insta-img" style="background-image: url(public/images/dessert-1.jpg);"></a>
-				<a href="#" class="insta-img" style="background-image: url(public/images/dessert-2.jpg);"></a>
-				<a href="#" class="insta-img" style="background-image: url(public/images/dish-9.jpg);"></a>
-				<a href="#" class="insta-img" style="background-image: url(public/images/dish-2.jpg);"></a>
-			</div>
-		</div>
-		<div class="col-md-3 col-pb-sm">
-			<h2>Newsletter</h2>
-			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia</p>
-			<div class="subscribe text-center">
-				<div class="form-group">
-					<input type="text" class="form-control text-center" placeholder="Enter Email address">
-					<input type="submit" value="Subscribe" class="btn btn-primary btn-custom">
-				</div>
+			<div id="fb-root"></div>
+			<div class="col-md-4 col-pb-sm">
+				<h2><a href="https://web.facebook.com/Poissonnerie-du-Port-Chez-Elise-274291612588893/"><i class="icon-facebook4"></i></a> Facebook</h2>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.0';
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-page" 
+  					data-href="https://web.facebook.com/Poissonnerie-du-Port-Chez-Elise-274291612588893/"
+  					data-heigth="600"
+  					data-tabs="timeline, events, message"
+  					data-hide-cover="false"
+  					data-show-facepile="false"
+  					data-adapt-container-width="true">
+  				<blockquote cite="https://web.facebook.com/Poissonnerie-du-Port-Chez-Elise-274291612588893/" class="fb-xfbml-parse-ignore"><a href="https://web.facebook.com/Poissonnerie-du-Port-Chez-Elise-274291612588893/">Poissonnerie du Port &quot;Chez Elise&quot;</a></blockquote>
+  				</div>
 			</div>
 		</div>
 	</div>
