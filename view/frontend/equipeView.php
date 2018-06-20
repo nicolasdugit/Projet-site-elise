5e0fcbefa1ee9f52d9c44b099071911fa0132826
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-20 09:41:46 +0700

    modifi header page equipe et rajout titre h4

--- view/frontend/equipeView.php
+++ view/frontend/equipeView.php
@@ -1,7 +1,7 @@
 <?php $title = 'Bienvenue à la Poissonnerie du Port Chez Elise' ?>
 <?php $description = 'Bienvenue à la poissonnerie du port Chez Élise. Élise vous accueille dans sa poissonnerie située à Lanton sur le port de Cassy, au cœur du bassin d\'Arcachon' ?>
 <?php $keywords = 'Poissonnerie du Port Chez Elise' ?>
-
+<?php $pageTitle = 'Notre Equipe' ?>
 
 <?php ob_start(); ?>
 
@@ -11,8 +11,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
-				<h2>Notre Equipe</h2>
-				<p> la Poissonnerie, l’esprit d’équipe est très important. Ici, on travaille dans la joie et la bonne humeur. Toute l’équipe est à votre disposition pour vous servir et vous conseiller tout au long de l’année.</p>
+				<h4> la Poissonnerie, l’esprit d’équipe est très important. Ici, on travaille dans la joie et la bonne humeur. Toute l’équipe est à votre disposition pour vous servir et vous conseiller tout au long de l’année.</h4>
 			</div>
 		</div>
 	</div>
