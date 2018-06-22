1a46abf75590d64553c19c63dba1fe76459840cd
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-22 16:49:14 +0700

    mise en place contenu page plats cuisinés

--- view/frontend/platsCuisineView.php
+++ view/frontend/platsCuisineView.php
@@ -11,7 +11,115 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
-				<h4>Pas le temps de cuisiner ? Choisissez parmi nos plats cuisinés maison. Brandade de morue, paella, soupe de poisson... Tous les jours, un large choix !</h4>
+				<span class="icon"><i class="flaticon-cutlery"></i></span>
+				<h2>Pas le temps de cuisiner ? Choisissez parmi nos plats cuisinés maison. Tous les jours, un large choix !</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/salade-de-morue.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">BRANDADE ET SALADE DE MORUE</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/paella.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">PAELLA DE LA MER</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/saumon.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">SAUMON GRAVLAX</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/fish-soup.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">SOUPE DE POISSON</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/zarzuela.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">ZARZUELA</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/morue-catalane.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">MORUE À LA CATALANE</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/curry-poisson.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">CURRY DE POISSON</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/burger.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">BURGER DU BASSIN</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 animate-box">
+				<div class="dish-wrap">
+					<div class="wrap">
+						<div class="dish-img" style="background-image: url(public/images/salade-de-moule.jpg);"></div>
+						<div class="desc">
+							<h2><a href="#">MOULES MARINIERES</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 animate-box intro-heading">
+				<blockquote>
+					<h4>Nous varions notre gamme de plats cuisinés chaque semaine.
+						<hr>
+						Certains produits ne sont pas fait maison comme les anchois marinés, les accras de morue, la salade d’écrevisse, mais ce sont égalements des produits artisanaux et de qualité.
+						<hr>
+						Nous avons l’objectif de continuer à développer et étendre notre gamme traiteur afin de répondre à la demande croissante de la clientèle. 
+						<hr>
+						Envie d’un plat cuisiné spécifique ? Besoin de nos services traiteur pour une réception ?
+						<br>
+						<a href="index.php?page=contact">N'hésitez pas à nous contacter</a></h4>
+				</blockquote>
 			</div>
 		</div>
 	</div>
