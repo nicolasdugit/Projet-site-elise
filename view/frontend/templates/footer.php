78d9c4c88d4fd06b2408f3ec66451cd0f9c0fb9a
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-22 17:58:41 +0700

    modification font-size footer

--- view/frontend/templates/footer.php
+++ view/frontend/templates/footer.php
@@ -2,18 +2,16 @@
 	<div id="footer">
 		<div class="container">
 			<div class="row">
-				<hr>
-				<div class="col-md-12 text-center">
-					<h4>Mentions Légales</h4>
+				<div class="col-md-12 text-center" style="font-size: 0.8em;">
+					<h2>Mentions Légales</h2>
 					<p>Entreprise Individuelle MADAME ELISE DUQUESNE (Poissonnerie du Port) <br>
 					529 633 885 R.C.S. BORDEAUX <br>
 					12 CHE DU PORT 33138 LANTON<br>05 57 70 70 49 <br>
 					Responsable éditorial : Poissonnerie du Port Chez Elise <br>
 					Hébergeur : 1&1 Internet SARL - 7, place de la Gare- BP 70109 - 57201 Sarreguemines Cedex - France <br>
 					Webmaster : <a href="http://www.fr.nicolasduquesne.com">Nicolas DUQUESNE</a> <br>
-
 					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						<span class="block">&copy;Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+						<span style="font-size: 1em; class="block">&copy;Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 						<br></span> 
 					</p>
 					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
