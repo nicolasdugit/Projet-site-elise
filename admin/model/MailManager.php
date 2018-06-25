d4469dcae983428ec1cfd43b5596905e2fd5eb26
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-25 15:07:45 +0700

    initialisation de ces modeles

---
+++ admin/model/MailManager.php
@@ -0,0 +1,9 @@
+<?php
+namespace Poissonneriedupost\Elise\Backend\Model;
+
+require_once("Manager.php");
+
+class MailManager extends Manager
+{
+	
+}
---
+++ admin/model/UserManager.php
@@ -0,0 +1,9 @@
+<?php
+namespace Poissonneriedupost\Elise\Backend\Model;
+
+require_once("Manager.php");
+
+class UserManager extends Manager
+{
+	
+}
