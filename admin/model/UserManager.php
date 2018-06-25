d53ae8b24c752b5ac2b2c7e7a3fa2c2d5e9b75d5
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-25 15:13:14 +0700

    creation function getUser dasn usermanager

--- admin/model/UserManager.php
+++ admin/model/UserManager.php
@@ -5,5 +5,13 @@ require_once("Manager.php");
 
 class UserManager extends Manager
 {
-	
+	public function getUser($name) 
+	{
+		$db = $this->dbConnect();
+		 $req = $db->prepare('SELECT * FROM boot-users WHERE name = ?');
+        $req->execute(array($name));
+        $affectedLines = $req->fetch();
+        
+        return $affectedLines;
+	}
 }
