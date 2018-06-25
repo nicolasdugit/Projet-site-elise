55f690e3aa79a4981f2385ad954a4cbc0f2c4d25
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-25 11:57:32 +0700

    initialisation index.php et templateAdmin.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,21 @@
 <?php
-	require('view/templates/leftSideBar.php');
-	require('view/templates/topBar.php');
-?>
+session_start();
+require('controller/backend.php');
+
+try {
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == '') {
+			# code...
+		}
+		elseif ($_GET['action'] == '') {
+			# code...
+		}
+	}
+	else {
+		adminHome();
+	}
+}
+catch(Exception $e) {
+    $errorMessage = $e->getMessage();
+    require('view/errorView.php');
+}
---
+++ admin/view/templates/templateAdmin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		
+	</head>
+	<body class="nav-md">
+		<div class="container body">
+			<div class="main_container">
+				<?php
+					require('view/templates/leftSideBar.php');
+					require('view/templates/topBar.php');
+				?>
+			</div>
+		</div>
+	</body>
+</html>
