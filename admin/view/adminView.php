b0bdf045d42c2d9f9774b96245102e106c73fc9d
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-25 17:43:35 +0700

    creation widgetMail

--- admin/view/adminView.php
+++ admin/view/adminView.php
@@ -1,7 +1,7 @@
 <?php $title = 'PAGE ADIMN -- Poissonnerie du Port Chez Elise'; ?>
 
 <?php ob_start(); ?>
-
+	<?php require('view/templates/widgetMail.php'); ?>
 
 <?php $content = ob_get_clean(); ?>
 
