e730bdd7efbf2aecfc9d5ccc9dcf089edeb45751
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-25 12:20:36 +0700

    ajout script js dans templateAdmin.php et corrections des liens relatifs

--- admin/view/templates/topBar.php
+++ admin/view/templates/topBar.php
@@ -1,3 +1,4 @@
+
 <div class="top_nav">
 	<div class="nav_menu">
 		<nav>
@@ -7,7 +8,7 @@
 			<ul class="nav navbar-nav navbar-right">
 				<li class="">
 					<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-						<img src="images/img.jpg" alt="">John Doe
+						<img src="view/images/img.jpg" alt="">John Doe
 						<span class=" fa fa-angle-down"></span>
 					</a>
 					<ul class="dropdown-menu dropdown-usermenu pull-right">
@@ -31,7 +32,7 @@
 					<ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
 						<li>
 							<a>
-								<span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+								<span class="image"><img src="view/images/img.jpg" alt="Profile Image" /></span>
 								<span>
 									<span>John Smith</span>
 									<span class="time">3 mins ago</span>
