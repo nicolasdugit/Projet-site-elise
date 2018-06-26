27d667ec45ed85429c905073498f7620a90ccdab
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-26 11:08:45 +0700

    modification errorView et cration d'un templateError

--- admin/view/errorView.php
+++ admin/view/errorView.php
@@ -2,16 +2,6 @@
 
 <?php ob_start(); ?>
 
-<div class="right_col" role="main">
-    <div class="">
-        <div class="page-title">
-            <div class="title_left">
-                <h3><?= $errorMessage ?></h3>
-                <p><a href="index.php">Retour la page Admin</a></p>
-            </div>
-        </div>
-    </div>
-</div>
 <?php $content = ob_get_clean(); ?>
 
-<?php require('templates/templateAdmin.php'); ?>
+<?php require('templates/templateError.php'); ?>
