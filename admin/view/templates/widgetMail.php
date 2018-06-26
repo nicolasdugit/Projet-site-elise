3ccb7c598ac73368ad23c1c4699f9ec33774b6ca
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-26 15:47:56 +0700

    affichage date amélioré

--- admin/view/templates/widgetMail.php
+++ admin/view/templates/widgetMail.php
@@ -26,8 +26,8 @@
 						<?php foreach (array_slice($mails, 0, 5) as $mail) : ?>
 							<article class="media event">
 								<a href="index.php?page=inbox&amp;id=<?= $mail['id'] ?>" class="pull-left date">
-									<p class="month"><?=$mail['mail_date'] ?></p>
-									<p class="day">23</p>
+									<p class="month"><?= $mail['mail_date'] ?></p>
+									<p class="day"><?=$mail['mail_date'][8],$mail['mail_date'][9]?></p>
 								</a>
 								<div class="media-body">
 									<a class="title" href="index.php?page=inbox&amp;id=<?= $mail['id'] ?>"><?=$mail['mail_name'] ?> </a>
