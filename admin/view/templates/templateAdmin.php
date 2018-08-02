800510b615c1cdc57cfd6830df4365ee088e6913
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-08-02 15:18:47 +0700

    suppression de tous les dossiers vendors inutiles

--- admin/view/templates/templateAdmin.php
+++ admin/view/templates/templateAdmin.php
@@ -12,26 +12,7 @@
 		<link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 		<!-- Font Awesome -->
 		<link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-		<!-- NProgress -->
-		<link href="vendors/nprogress/nprogress.css" rel="stylesheet">
-		<!-- iCheck -->
-		<link href="vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-		<!-- bootstrap-progressbar -->
-		<link href="vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-		<!-- JQVMap -->
-		<link href="vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
-		<!-- bootstrap-daterangepicker -->
-		<link href="vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-		<!-- bootstrap-wysiwyg -->
-		<link href="vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
-		<!-- bootstrap-datetimepicker -->
-		<link href="vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
-		<!-- Ion.RangeSlider -->
-		<link href="vendors/normalize-css/normalize.css" rel="stylesheet">
-		<link href="vendors/ion.rangeSlider/css/ion.rangeSlider.css" rel="stylesheet">
-		<link href="vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css" rel="stylesheet">
-		<!-- Bootstrap Colorpicker -->
-		<link href="vendors/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+
 		<!-- Dropzone.js -->
 		<link href="vendors/dropzone/dist/min/dropzone.min.css" rel="stylesheet">
 		<!-- Custom Theme Style -->
@@ -48,56 +29,12 @@
 		<script src="vendors/jquery/dist/jquery.min.js"></script>
 		<!-- Bootstrap -->
 		<script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-		<!-- FastClick -->
-		<script src="vendors/fastclick/lib/fastclick.js"></script>
-		<!-- NProgress -->
-		<script src="vendors/nprogress/nprogress.js"></script>
-		<!-- Chart.js -->
-		<script src="vendors/Chart.js/dist/Chart.min.js"></script>
-		<!-- gauge.js -->
-		<script src="vendors/gauge.js/dist/gauge.min.js"></script>
-		<!-- bootstrap-progressbar -->
-		<script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-		<!-- iCheck -->
-		<script src="vendors/iCheck/icheck.min.js"></script>
-		<!-- Skycons -->
-		<script src="vendors/skycons/skycons.js"></script>
-		<!-- Flot -->
-		<script src="vendors/Flot/jquery.flot.js"></script>
-		<script src="vendors/Flot/jquery.flot.pie.js"></script>
-		<script src="vendors/Flot/jquery.flot.time.js"></script>
-		<script src="vendors/Flot/jquery.flot.stack.js"></script>
-		<script src="vendors/Flot/jquery.flot.resize.js"></script>
-		<!-- Flot plugins -->
-		<script src="vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-		<script src="vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-		<script src="vendors/flot.curvedlines/curvedLines.js"></script>
-		<!-- DateJS -->
-		<script src="vendors/DateJS/build/date.js"></script>
-		<!-- JQVMap -->
-		<script src="vendors/jqvmap/dist/jquery.vmap.js"></script>
-		<script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-		<script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-		<!-- bootstrap-daterangepicker -->
-		<script src="vendors/moment/min/moment.min.js"></script>
-		<script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>	
+		
 		<!-- bootstrap-wysiwyg -->
 		<script src="vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
 		<script src="vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
 		<script src="vendors/google-code-prettify/src/prettify.js"></script>
-		<!-- bootstrap-daterangepicker -->
-		<script src="vendors/moment/min/moment.min.js"></script>
-		<script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-		<!-- bootstrap-datetimepicker -->    
-		<script src="vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-		<!-- Ion.RangeSlider -->
-		<script src="vendors/ion.rangeSlider/js/ion.rangeSlider.min.js"></script>
-		<!-- Bootstrap Colorpicker -->
-		<script src="vendors/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
-		<!-- jquery.inputmask -->
-		<script src="vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
-		<!-- jQuery Knob -->
-		<script src="vendors/jquery-knob/dist/jquery.knob.min.js"></script>
+		
 		<!-- Dropzone.js -->
 		<script src="vendors/dropzone/dist/min/dropzone.min.js"></script>
 		<!-- Custom Theme Scripts -->
